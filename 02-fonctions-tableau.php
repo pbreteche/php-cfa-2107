9a424015cb607874aac2e3b90114eee1358270b3
Pierre Bretéché <pbreteche@dawan.fr>
2021-07-26 14:45:58 +0200

    autres fonctions de tableau

--- 02-fonctions-tableau.php
+++ 02-fonctions-tableau.php
@@ -22,3 +22,89 @@ array_push($fruits, 'datte');
 array_unshift($fruits, 'pomme'); //insère au début et décale les éléments
 $fruits[] = 'poire';
 
+// extraction
+array_pop($fruits);
+array_shift($fruits);
+// extraction d'une tranche (sans suppression)
+$bananeCerise = array_slice($fruits, 1, 2);
+// extraction d'une tranche (avec suppression/remplacement)
+array_splice($fruits, 1, 2, ['pomme', 'poire']);
+// suppression
+unset($fruits[count($fruits) - 1]);
+
+foreach ($fruits as $i => $fruit) {
+    echo $fruit.'/'.$i."\n";
+}
+
+while ($fruit = current($fruits)) {
+    echo $fruit.'/'.key($fruits)."\n";
+    next($fruits); // end(), reset()
+}
+
+// applique une fonction sur chaque élément du tableau
+array_walk($fruits, function ($fruit) {
+    echo $fruit."\n";
+});
+
+// retourne un tableau qui le résultat d'un appel
+// de fonction sur chacun des éléments
+$upperFruits = array_map('strtoupper', $fruits);
+print_r($upperFruits);
+
+$stringFruits = array_reduce($fruits, function ($fruit, $accumulator) {
+   return $accumulator.' - '.$fruit;
+});
+echo $stringFruits."\n";
+
+$oddFruits = array_filter($fruits, function($fruit) {
+    return strlen($fruit) % 2;
+});
+print_r($oddFruits);
+
+function traitementAvecOptions($options = [])
+{
+    $defaultOptions = [
+        'height' => 120,
+        'width' => 160,
+        'color' => '#abcdef',
+    ];
+
+    $parametres = array_merge($defaultOptions, $options);
+    print_r($parametres);
+}
+
+traitementAvecOptions(['height' => 80, 'border' => 'none']);
+
+print_r(array_values($fruits));
+print_r(array_keys($fruits));
+
+// Fonctions de tri:
+// sort, rsort, asort, ksort, usort, uksort, uasort, krsort, ...
+rsort($fruits); // tri inverse sur les valeurs
+print_r($fruits);
+asort($fruits); // tri sur les valeurs, maintient l'association des clés
+print_r($fruits);
+ksort($fruits); // tri sur les clés
+print_r($fruits);
+
+// tri personnalisé
+// tri alphabétique en lisant à l'envers
+usort($fruits, function ($a, $b) {
+    return strrev($a) <=> strrev($b);
+});
+
+function estPalindrome(string $a): bool
+{
+    $letters = mb_str_split($a);
+    krsort($letters);
+    $reverseA = implode('', $letters);
+
+    return $a == $reverseA;
+}
+
+print_r(estPalindrome('été'));
+
+$fruits = ['cerise', 'pomme', 'citron'];
+$couleurs = ['rouge', 'vert', 'jaune'];
+
+print_r(array_combine($fruits, $couleurs));
